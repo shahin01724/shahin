e7e890cdf2a88efce82b2242e833b8be384f828a
shahin01724 <sharifulislam2884@gmail.com>
2025-05-08 00:59:41 +0600

    Update product.js

--- product.js
+++ product.js
@@ -1,6 +1,6 @@
-        // Sample product data with color images and free delivery flag
-        const products = [
-                {
+// Sample product data with color images and free delivery flag
+const products = [
+    {
         id: 1,
         title: "Black Color Cap",
         slug: "black-color-cap",
@@ -13,22 +13,22 @@
             "https://i.postimg.cc/FKcKJkvL/Cuatomizes-Cap-Black-4.jpg",
             "https://i.postimg.cc/gJ86dBj1/Cuatomizes-Cap-Black-5.jpg"
         ],
-                description: "Comfortable and stylish men's casual t-shirt made from 100% cotton. Available in various colors and sizes.",
-                colors: ["Red", "Blue", "Black", "White", "Green"],
-                colorImages: {
-                    "Red": "https://static.vecteezy.com/system/resources/thumbnails/008/533/992/small_2x/red-cap-mockup-cutout-file-png.png",
-                    "Blue": "https://www.shutterstock.com/image-photo/baseball-cap-isolated-on-white-600nw-610601909.jpg",
-                    "Black": "https://t3.ftcdn.net/jpg/02/72/20/82/360_F_272208218_pS249ZTq8cu3yknECXm0K3U8UtOIBVvr.jpg",
-                    "White": "https://3.imimg.com/data3/AY/WT/MY-13475627/white-cap.jpg",
-                    "Green": "https://m.media-amazon.com/images/I/613zrbW8OrL._AC_SL1001_.jpg"
-                },
-                colorCodes: {
-                    "Red": "#ff0000",
-                    "Blue": "#0000ff",
-                    "Black": "#000000",
-                    "White": "#ffffff",
-                    "Green": "Green"
-                },
+        description: "Comfortable and stylish men's casual t-shirt made from 100% cotton. Available in various colors and sizes.",
+        colors: ["Red", "Blue", "Black", "White", "Green"],
+        colorImages: {
+            "Red": "https://static.vecteezy.com/system/resources/thumbnails/008/533/992/small_2x/red-cap-mockup-cutout-file-png.png",
+            "Blue": "https://www.shutterstock.com/image-photo/baseball-cap-isolated-on-white-600nw-610601909.jpg",
+            "Black": "https://t3.ftcdn.net/jpg/02/72/20/82/360_F_272208218_pS249ZTq8cu3yknECXm0K3U8UtOIBVvr.jpg",
+            "White": "https://3.imimg.com/data3/AY/WT/MY-13475627/white-cap.jpg",
+            "Green": "https://m.media-amazon.com/images/I/613zrbW8OrL._AC_SL1001_.jpg"
+        },
+        colorCodes: {
+            "Red": "#ff0000",
+            "Blue": "#0000ff",
+            "Black": "#000000",
+            "White": "#ffffff",
+            "Green": "#00ff00"
+        },
         sizes: ["S", "M", "L", "XL"],
         stock: 15,
         deliveryFree: true
@@ -46,24 +46,24 @@
             "https://i.postimg.cc/FzRtQwsx/Customizes-Cap-Khaki-4.jpg",
             "https://i.postimg.cc/pXrxN5mt/Customizes-Cap-Khaki-5.jpg"
         ],
-                description: "Comfortable and stylish men's casual t-shirt made from 100% cotton. Available in various colors and sizes.",
-                colors: ["Red", "Blue", "Black", "White", "Green"],
-                colorImages: {
-                    "Red": "https://static.vecteezy.com/system/resources/thumbnails/008/533/992/small_2x/red-cap-mockup-cutout-file-png.png",
-                    "Blue": "https://www.shutterstock.com/image-photo/baseball-cap-isolated-on-white-600nw-610601909.jpg",
-                    "Black": "https://t3.ftcdn.net/jpg/02/72/20/82/360_F_272208218_pS249ZTq8cu3yknECXm0K3U8UtOIBVvr.jpg",
-                    "White": "https://3.imimg.com/data3/AY/WT/MY-13475627/white-cap.jpg",
-                    "Green": "https://m.media-amazon.com/images/I/613zrbW8OrL._AC_SL1001_.jpg"
-                },
-                colorCodes: {
-                    "Red": "#ff0000",
-                    "Blue": "#0000ff",
-                    "Black": "#000000",
-                    "White": "#ffffff",
-                    "Green": "Green"
-                },
+        description: "Comfortable and stylish men's casual t-shirt made from 100% cotton. Available in various colors and sizes.",
+        colors: ["Red", "Blue", "Black", "White", "Green"],
+        colorImages: {
+            "Red": "https://static.vecteezy.com/system/resources/thumbnails/008/533/992/small_2x/red-cap-mockup-cutout-file-png.png",
+            "Blue": "https://www.shutterstock.com/image-photo/baseball-cap-isolated-on-white-600nw-610601909.jpg",
+            "Black": "https://t3.ftcdn.net/jpg/02/72/20/82/360_F_272208218_pS249ZTq8cu3yknECXm0K3U8UtOIBVvr.jpg",
+            "White": "https://3.imimg.com/data3/AY/WT/MY-13475627/white-cap.jpg",
+            "Green": "https://m.media-amazon.com/images/I/613zrbW8OrL._AC_SL1001_.jpg"
+        },
+        colorCodes: {
+            "Red": "#ff0000",
+            "Blue": "#0000ff",
+            "Black": "#000000",
+            "White": "#ffffff",
+            "Green": "#00ff00"
+        },
         sizes: ["S", "M", "L"],
         stock: 8,
         deliveryFree: false
-    },
-        ];
+    }
+];
